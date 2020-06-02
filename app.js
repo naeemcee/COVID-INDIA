b3f1935feab77db4ed40d55094212b179a7c3b5f
Naeem <66157869+naeemcee@users.noreply.github.com>
2020-06-02 06:06:48 +0300

    Revert "daily delta added"
    This reverts commit b2b793a2d4c74ab14334703df12ad1015773cff8.

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
+import { someThing } from './render.js'
 
 let dropdownList = document.querySelector('#dd-list')
-let tableHeaderRow = document.querySelectorAll('#table-header-row')
 let dataTable = document.querySelector('#data-table')
 
-let newMaster = []              //holds 'cleaned up', sorted API data from both calls, with national summary as element 0
 let rawData = []                //holds copy of downloaded API data
-let addlData = []               // holds data from the second api call
-
+let masterData = []             //holds 'cleaned up' and sorted API data, including national summary as element 0
+let nationalSummary             //holds details of the state selected from dropdown list
 let activeState = 0             //index of the default state
 let updateTime = moment()
+let updateCounter = 0
 
-const intlFormat = 'en-US'      //number display format settings
-const shwoZeroState = false     //flag whether to show states with zero confirmed cases or not
+let url1 = 'https://api.covid19india.org/v2/state_district_wise.json'
+let url2 = 'https://api.covid19india.org/deaths_recoveries.json'
 
-let url1 = 'https://api.covid19india.org/data.json'                     //National time series, statewise stats and test counts	(NOT Working!!!)
-let url2 = 'https://api.covid19india.org/v2/state_district_wise.json'   //State-district-wise V2
-// let url3 = 'https://api.covid19india.org/deaths_recoveries.json'        //Deaths and Recoveries
-
-const numFormat = (num) => {
-    return Intl.NumberFormat(intlFormat).format(num)
-}
-
-const loadMasterData = () => {                          //load masterData from localstorage
-    const masterJSON = localStorage.getItem('masterdata')  
-    try {
-        return masterJSON ? JSON.parse(masterJSON) : []
-    } catch (e) {
-        return []
-    }
-}
 
 const getData = async (url) => {
     const response = await fetch(url)
@@ -39,6 +23,26 @@ const getData = async (url) => {
         throw new Error('Unable to fetch data.. please try later')
     }
 }
+
+getData(url1).then((apiData) => {
+    updateTime = moment()
+
+    rawData = {...apiData}                      //clone apiData
+    cleanupRawData()                            //cleanup and and save in masterData
+    populateDropdown()                          //populate drop down list
+
+    //populate INDIA summary totals
+    document.querySelector('#summary-block-confirmed').textContent = Intl.NumberFormat('en-US').format(masterData[0].confirmed)
+    document.querySelector('#summary-block-today').textContent = Intl.NumberFormat('en-US').format(masterData[0].newCases)
+    document.querySelector('#update-time').textContent = `Last updated: ${updateTime.format("DD MMM HH:mm")} (${updateTime.fromNow()})`
+    renderData (activeState, 'confirmed')       //render masterData
+
+    getAdditionalInfo()
+    
+}).catch((err) => {
+    console.log(`Error: ${err}`)
+    document.querySelector('#error-area').textContent = `<p class="error">Error (01) aquiring data. Please try again</p>`
+})
 
 const sortData = (dArray, key) => {
     if (key === 'name') {
@@ -53,19 +57,19 @@ const sortData = (dArray, key) => {
         })
     } else if (key === 'confirmed'){
         return dArray.sort ( (a, b) => {
-            if (Number(a.confirmed) > Number(b.confirmed)) {
+            if (a.confirmed > b.confirmed) {
                 return -1
-            } else if (Number(a.confirmed) < Number(b.confirmed)) {
+            } else if (a.confirmed < b.confirmed) {
                 return 1
             } else {
                 return 0
             }
         })
-    } else if (key === 'deltaconfirmed'){
+    } else if (key === 'newCases'){
         return dArray.sort ( (a, b) => {
-            if (a.deltaconfirmed > b.deltaconfirmed) {
+            if (a.newCases > b.newCases) {
                 return -1
-            } else if (a.deltaconfirmed < b.deltaconfirmed) {
+            } else if (a.neweCases < b.newCases) {
                 return 1
             } else {
                 return 0
@@ -75,86 +79,66 @@ const sortData = (dArray, key) => {
     return dArray
 }
 
-const cleanupRawData = () => {              //cleanup and save raw-data into masterData array, with a summary item as first element
-let natSummary = []
+const cleanupRawData = () => {                      //cleanup and save raw-data into master-data array, with a summary item as first element
+    let nationalConfirmedCases = 0
+    let nationalNewCases = 0
 
-for (let item in addlData.statewise) {
-
-    newMaster.push({
-        name: addlData.statewise[item].state,
-        confirmed: Number(addlData.statewise[item].confirmed),
-        deltaconfirmed: Number(addlData.statewise[item].deltaconfirmed),
-        deaths: Number(addlData.statewise[item].deaths),
-        deltadeaths: Number(addlData.statewise[item].deltadeaths),
-        recovered: Number(addlData.statewise[item].recovered),
-        deltarecovered: Number(addlData.statewise[item].deltarecovered),
-        active: addlData.statewise[item].confirmed - addlData.statewise[item].recovered - addlData.statewise[item].deaths,
-        deltaactive: addlData.statewise[item].deltaconfirmed - addlData.statewise[item].deltarecovered - addlData.statewise[item].deltadeaths
-    })
+    for (var index in rawData) {
+        let confirmedCases = 0
+        let newCases = 0
+        let stateDetails = []
         
-    if (item > 0) {
-        natSummary.push({
-            name: newMaster[item].name,
-            confirmed: newMaster[item].confirmed,
-            deltaconfirmed: newMaster[item].deltaconfirmed,
-            deaths: newMaster[item].deaths,
-            deltadeaths: newMaster[item].deltadeaths,
-            recovered: newMaster[item].recovered,
-            deltarecovered: newMaster[item].deltarecovered
-        })    
-    }
-}
-
-for (let item in newMaster) {           //get district details added to each state 
-    let distSummary = []
-
-    for (let j in rawData) {
-        if (newMaster[item].name.toLowerCase() == rawData[j].state.toLowerCase()) {
-            // newMaster[item].districtData = {...rawData[j].districtData}
-            //the above line is a simple one line replacement of the below for loop however, this copies the structure of the rawdata in terms of nested delta numbers within district data this is avoided now.
+        for (var j in rawData[index].districtData) {
+            confirmedCases += rawData[index].districtData[j].confirmed
+            newCases += rawData[index].districtData[j].delta.confirmed
             
-        for (let k in rawData[j].districtData) {
-            distSummary.push({
-                name: rawData[j].districtData[k].district,
-                confirmed: rawData[j].districtData[k].confirmed,
-                deltaconfirmed: rawData[j].districtData[k].delta.confirmed,
-                recovered: rawData[j].districtData[k].recovered,
-                deltarecovered: rawData[j].districtData[k].delta.recovered,
-                deaths: rawData[j].districtData[k].deceased,
-                deltadeaths: rawData[j].districtData[k].delta.deceased
+            stateDetails.push ({
+                name: rawData[index].districtData[j].district,
+                confirmed: rawData[index].districtData[j].confirmed,
+                newCases: rawData[index].districtData[j].delta.confirmed
             })
         }
-        newMaster[item].districtData = distSummary
-        } 
-    } 
-    if (!newMaster[item].districtData) {
-        newMaster[item].districtData = []
-    }
-}
 
-newMaster[0].name = "All States"
-newMaster[0].districtData = [...natSummary]
-sortData(newMaster, 'name')
-console.log('NEW MASTER >>', newMaster)
-
-// API data recociliation - for audit purpose only
-let distDeltaConfirmed = 0
-let i = 1       //exclude 'all states' item
-for (i ; i < newMaster.length; i++) {
-    for (let j in newMaster[i].districtData) {
-    distDeltaConfirmed += Number(newMaster[i].districtData[j].deltaconfirmed)
+        masterData.push ({
+            name: rawData[index].state,
+            confirmed: confirmedCases,
+            newCases: newCases,
+            details: stateDetails
+        })                                   
+        nationalConfirmedCases += confirmedCases
+        nationalNewCases += newCases   
     }
-}
-console.log(`New Cases > from State data: ${Number(newMaster[0].deltaconfirmed)}, from Dist data: ${distDeltaConfirmed}`)
+    
+    //sort masterData before adding 'all states' as the first element
+    sortData(masterData, 'name')
+    console.log('Alpha sorted masterData before adding all-states', masterData)
+    
+    //save the current state of masterData as the nationalSummary and sort based on confirmed cases
+    nationalSummary=JSON.parse(JSON.stringify(masterData))
+    
+    sortData(nationalSummary, 'confirmed')
+    console.log('nationalSummary sorted by confirmed cases', nationalSummary)
+
+    //add 'all states' item to masterData array at index 0
+    masterData.splice (0,0,{
+        name: "All States",
+        confirmed: nationalConfirmedCases,
+        newCases: nationalNewCases,
+        details: nationalSummary
+    })
+
+console.log ('alpha sorted masterData with ALL STATES at top', masterData)
 }
 
 const updateRefreshTime = () => {
-    
-    if ((moment() - updateTime) > 900000 ) {
-        location.reload(true)           //re-load page if time elapsed more than 15 mins
-    } else {
+    if (updateCounter < 10) {
         document.querySelector('#update-time').innerHTML = `Last updated: ${updateTime.format("DD MMM HH:mm")} <span class="time-highlight">(${updateTime.fromNow()})</span>`
+        updateCounter += 1
+    } else {
+        updateCounter = 0       //counter is reset as part of page reload.. so not required.
+        location.reload(true)
     }
+    console.log('time lapsed updated')
 }
 
 const renderData = (stateIndex, sortBy) => {
@@ -162,144 +146,96 @@ const renderData = (stateIndex, sortBy) => {
     let dataRow = ''
     let newCases = ''
     let titleLabel = (activeState == 0 ) ? 'State / UT' : 'District / Area'
-    
-    if (sortBy == 'name') titleLabel = titleLabel + ' ▼'
 
-    if (sortBy !== 'confirmed') document.querySelector('#sortby-total').textContent = 'TOTAL'
-    else document.querySelector('#sortby-total').textContent = 'TOTAL' + ' ▼'
-    
-    if (sortBy !== 'deltaconfirmed') document.querySelector('#sortby-today').textContent = 'TODAY'
-    else document.querySelector('#sortby-today').textContent = 'TODAY' + ' ▼'
-    
+    //do something and clear the data table
     dataTable.innerHTML = ''
-    document.querySelector('#summary-block').scrollTop = 0
+
     document.querySelector('.table-header-label1').textContent = titleLabel
-
-    stateDetails = [...newMaster[stateIndex].districtData]
-
-    if (sortBy == 'deltaconfirmed') {
+    stateDetails = masterData[stateIndex].details
+    if (sortBy == 'newCases') {
         stateDetails = sortData (stateDetails, 'confirmed')      //first sort by confirmed
     }
     stateDetails = sortData (stateDetails, sortBy)              // then by other key
 
     console.log('stateDetails Array>>', stateDetails)
     
-    for (var item in stateDetails) {
-        //render data table
-        if (stateDetails[item].confirmed == 0 && !shwoZeroState) continue  //eliminate zero case states from display
+    // table rendered in HTML, so no need of the following lines
+    // dataRow = `<tr class="table-header-row"><th class="table-header-col1" colspan="2">${titleLabel}</th><th class="table-header-col2">Total Cases</th><th class="table-header-col3">New Cases</th></tr>`
+    // dataTable.innerHTML = dataRow
 
-        newCases = Number(stateDetails[item].deltaconfirmed) ? '+' + numFormat(stateDetails[item].deltaconfirmed) : ''
+    for (var item in stateDetails) {
+        //render data list
+        newCases = stateDetails[item].newCases ? '+' + Intl.NumberFormat('en-US').format(stateDetails[item].newCases) : ''
         dataRow = `<tr class="t-row"><td class="td-sl-no">${Number(item)+1}</td><td class="td-name">${stateDetails[item].name}</td><td class="td-confirmed">${Intl.NumberFormat('en-US').format(stateDetails[item].confirmed)}</td><td class="td-new">${newCases}</td></tr>`
         dataTable.insertAdjacentHTML("beforeend", dataRow)
     } 
+    
+    // //update refresh time past from now info
+    // document.querySelector('#update-time').innerHTML = `Last updated: ${updateTime.format("DD MMM HH:mm")} <span class="time-highlight">(${updateTime.fromNow()})</span>`
+    updateRefreshTime()
 
     // populate the sate totals element on page
-    document.querySelector('#dd-bar-confirmed').textContent = numFormat(newMaster[activeState].confirmed)
-    document.querySelector('#dd-bar-today').textContent = `+${numFormat(newMaster[activeState].deltaconfirmed)}`
-
-    document.querySelector('#summary-block-recovered').textContent = numFormat(newMaster[0].recovered)
-    document.querySelector('#summary-block-fatality').textContent = numFormat(newMaster[0].deaths)
-    document.querySelector('#summary-block-active').textContent = numFormat(newMaster[0].active)
-    document.querySelector('#summary-block-delta-recovered').textContent = '+' + numFormat(newMaster[0].deltarecovered)
-    document.querySelector('#summary-block-delta-fatality').textContent = '+' + numFormat(newMaster[0].deltadeaths)
-    
-    document.querySelector('#summary-block-delta-confirmed').textContent = '+' + numFormat(newMaster[0].deltaconfirmed)
-    document.querySelector('#summary-block-delta-active').textContent = '+' + numFormat(newMaster[0].deltaactive)
-
-    document.querySelector('#summary-block-confirmed').textContent = numFormat(newMaster[0].confirmed)
-    document.querySelector('#summary-block-today').textContent = numFormat(newMaster[0].deltaconfirmed)
-    
-    updateRefreshTime()
+    document.querySelector('#dd-bar-confirmed').textContent = Intl.NumberFormat('en-US').format(masterData[activeState].confirmed)
+    document.querySelector('#dd-bar-today').textContent = Intl.NumberFormat('en-US').format(masterData[activeState].newCases)    
 }
 
 const populateDropdown = () => {
     var optionString = ''
 
-    for (var item in newMaster) {
-        if (newMaster[item].confirmed == 0 && !shwoZeroState) continue  //eliminate zero case states from dropdown
+    for (var item in masterData) {
         if (item == activeState)  
-            optionString += `<option value="${item}" selected="true">${newMaster[item].name}</option>`
+            optionString += `<option value="${item}" selected="true">${masterData[item].name}</option>`
         else
-            optionString += `<option value="${item}" >${newMaster[item].name}</option>`
+            optionString += `<option value="${item}" >${masterData[item].name}</option>`
     } 
     dropdownList.innerHTML = optionString
 }
 
 const getAdditionalInfo = () => {                   //call this function to fetch additional info from url2
-    getData(url1).then((apiData) => {               //get fatalities and recovery database
-    addlData = {...apiData}                         //duplicate returned data to addlData
+    getData(url2).then((apiData) => {               //get fatalities and recovery database
+    let fatalCases = 0
+    let recoveredCases = 0
+    let unknownOutcome = 0
+
+    console.log(apiData)
+    //cleanup data and retrieve required info. that's something for another day!
     
-    cleanupRawData()                            //cleanup and and save in masterData
-    populateDropdown()                          //populate drop down list
-    renderData (activeState, 'confirmed')       //render masterData
+    //iniitally capturing only overall, national level deaths and recoveries
+    for (var item in apiData.deaths_recoveries) {
+        if (String(apiData.deaths_recoveries[item].patientstatus).toLowerCase() == 'deceased') {
+            fatalCases += 1
+        } else if (String(apiData.deaths_recoveries[item].patientstatus).toLowerCase() == 'recovered') {
+            recoveredCases += 1
+        }
+         else {
+             unknownOutcome += 1
+        }
+    }
+    document.querySelector('#summary-block-recovered').textContent = Intl.NumberFormat('en-US').format(recoveredCases)
+    document.querySelector('#summary-block-fatality').textContent = Intl.NumberFormat('en-US').format(fatalCases)
+    document.querySelector('#summary-block-active').textContent = Intl.NumberFormat('en-US').format(masterData[0].confirmed - recoveredCases - fatalCases)
+    
+    
+    console.log(`Recovered: ${recoveredCases}, Fatality: ${fatalCases}, Unknown: ${unknownOutcome}`)
 
 }).catch((err) => {
     console.log(`Error: ${err}`)
-    document.querySelector('#error-area').innerHTML = `<p class="error">error connecting to one of the data sources. recovery / fatality count unavailable.. please try again later</p>`
+    document.querySelector('#error-area').textContent = `<p class="error">Error (02) aquiring data. Please try again</p>`
 })
+
 }
-
-const getMainData = () => {
-
-    getData(url2).then((apiData) => {
-        updateTime = moment()
-        rawData = {...apiData}                      //clone apiData
-        getAdditionalInfo()                         // make the second API call
-
-    }).catch((err) => {
-        console.log(`Error: ${err}`)
-        document.querySelector('#error-area').innerHTML = `<p class="error">Oops.. error connecting to data sources ... please try again later</p>`
-    })
-}
-
-getMainData()
 
 dropdownList.addEventListener('change',(e) => {
-    console.log(dropdownList.value, newMaster[dropdownList.value].name)
+    console.log(dropdownList.value, masterData[dropdownList.value].name)
     
     activeState = dropdownList.value
     renderData (activeState, 'confirmed')
     
 })
 
-document.querySelector('#table-header-row').addEventListener('click', (e) => {
-    // console.log(e.srcElement.textContent)
-    let sortkey = ''
-    switch(e.srcElement.textContent.toLowerCase())
-     {
-        case 'total': 
-            sortkey = 'confirmed'
-            break
-        case 'today':
-            sortkey = 'deltaconfirmed'
-            break
-        default:
-            sortkey = 'name'
-    }
-    
-    console.log('sorting by: ', sortkey)
+//update last refresh time every 1 minute
+setInterval(updateRefreshTime, 60000)
 
-    renderData(activeState, sortkey)
-})
+console.log (someThing) //troubleshooting import / export
 
-/* document.querySelector('#sortby-name').addEventListener('click', (e) => {
-    console.log(e.currentTarget.innerText)
-    renderData(activeState, 'name')
-})
- 
-document.querySelector('#sortby-total').addEventListener('click', (e) => {
-    console.log(e.currentTarget.innerText)
-    renderData(activeState, 'confirmed')
-})
-
-document.querySelector('#sortby-today').addEventListener('click', (e) => {
-    console.log(e.currentTarget.innerText)
-    renderData(activeState, 'deltaconfirmed')
-}) */
-
-/* 
-window.onscroll = () => {
-    console.log(document.body.scrollTop)
-} */
-
-setInterval(updateRefreshTime, 60000)   //update last refresh time every 1 minute
+export { masterData }
--- render.js
+++ render.js
@@ -1,3 +1,4 @@
+import { masterData } from './app.js'
 
 let someThing = 1000
 // console.log('from render.js >> ', masterData)
